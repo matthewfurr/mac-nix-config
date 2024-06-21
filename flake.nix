8a50455f6df3dac76c52bc988d63b8b5abd219ee
Matthew Furr <5639047+matthewfurr@users.noreply.github.com>
2024-06-21 11:41:40 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "My MacOS Nix Config";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; # nixos-23.11
+
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    pwnvim.url = "github:zmre/pwnvim";
+  };
+  outputs = inputs@{ nixpkgs, home-manager, darwin, pwnvim, ... }: {
+    darwinConfigurations.Demos-Virtual-Machine = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { system = "aarch64-darwin"; };
+      modules = [
+        ./modules/darwin
+        home-manager.darwinModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = { inherit pwnvim; };
+            users.demo.imports = [ ./modules/home-manager ];
+          };
+        }
+      ];
+    };
+  };
+}
